045ba48d28f09d06ec76b7e900965946934c7010
Reza Iranbakhsh <reza.iranb@gmail.com>
2022-08-04 16:45:53 +0430

    Add files via upload

---
+++ mainchess.py
@@ -0,0 +1,50 @@
+from xmlrpc.client import FastMarshaller
+import chess
+import random
+
+def showBoard(board):
+    print("<------------->")
+    print(board)
+    print("<------------->")
+
+def makeMove(board, whoAmI, chuckToMove):
+    if chuckToMove:
+        #its Chuck move
+        board.push(chess.Move.from_uci(str(chuckMove(board, whoAmI))))
+    else:
+        #its Player move
+        board.push(chess.Move.from_uci(playerMove(board, whoAmI)))
+def chuckMove(board, whoAmI):
+    for move in board.legal_moves:
+        isTheMove = random.randint(0, 2)
+        if isTheMove:
+            return move
+def playerMove(board, whoAmI):
+    move = ""
+    isValidMove = False
+    print("Choose Your Move Wisely : ")
+    while not isValidMove:
+        move = input()
+        if chess.Move.from_uci(move) in board.legal_moves:
+            isValidMove = True
+            print("Finally")
+            return move
+        else:
+            print("I Said Choose Your Move Wisely : ")
+            
+    return move
+
+print("<-------- Welcome To LC0 -------->")
+whoAmI = not bool(int(input("LC0 : Choose Your Color \n\tWhite => 1\n\tBlack => 0\nYou :  ")))
+board = chess.Board()
+chuckToMove = whoAmI
+gameIsOn = True
+checkmate = False
+stalemate = False
+while gameIsOn:
+    showBoard(board)
+    makeMove(board, whoAmI, chuckToMove)
+    if checkmate or stalemate:
+        gameIsOn = False
+        break
+    chuckToMove = not chuckToMove
