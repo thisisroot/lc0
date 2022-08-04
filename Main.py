5786d7a12d48d966c4d525bc9b87c1fe4a52be42
Rvaziri <rezavnejad@gmail.com>
2022-08-04 19:17:47 +0430

    several bug fixes

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ import pygame as pg
 WIDTH = HEIGHT = 512
 DIMENSION = 8
 SQ_SIZE = HEIGHT // WIDTH
-MAX_FPS = 30
+MAX_FPS = 15
 IMAGES = {}
 
 def Images():
@@ -21,22 +21,24 @@ def Images():
         IMAGES[piece] = pg.transform.scale(pg.image.load("pieces/Black/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
     for piece in piecesW:
         IMAGES[piece] = pg.transform.scale(pg.image.load("pieces/White/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+
+def d_game_state(screen, board):
+    d_board(screen)
+    d_piece(screen, board)
     
-def screen(board):
-    pg.init()
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
-    clock = pg.time.Clock
-    screen.fill(pg.Color("white"))
-    Images()
-    activeness = True
-    while activeness:
-        for e in pg.event.get():
-            if e.type == pg.QUIT:
-                activeness = False
-        clock.tick(MAX_FPS)
-        pg.display.flip()
-
-
+def d_board(screen):
+    colors = [pg.Color("white"), pg.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors [((r + c) % 2)]
+            pg.draw.rect(screen, color, pg.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+    
+    
+    
+def d_piece(screen, board):pass
+    
 def main():
     print("<-------- Welcome To LC0 -------->")
     whoAmI = not bool(int(input("LC0 : Choose Your Color \n\tWhite => 1\n\tBlack => 0\nYou :  ")))
@@ -45,28 +47,31 @@ def main():
 
     pg.init()
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    clock = pg.time.Clock
+    clock = pg.time.Clock()
     screen.fill(pg.Color("white"))
     Images()
     activeness = True
-    while cg.gameIsOn and activeness:
+    while activeness:
 
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 activeness = False
+        d_game_state(screen, cg.board)
         clock.tick(MAX_FPS)
         pg.display.flip()
 
 
+
+
         #showBoard(cg.board)
-        if whoAmI == cg.whiteToMove:
-            lc = Chuck(cg.board, whoAmI)
-            cg.makeMove(lc.makeMove())
-        else:
-            p = Player(cg.board, not whoAmI)
-            cg.makeMove(p.makeMove())
-        if cg.isGameOver():
-            break
+        # if whoAmI == cg.whiteToMove:
+        #     lc = Chuck(cg.board, whoAmI)
+        #     cg.makeMove(lc.makeMove())
+        # else:
+        #     p = Player(cg.board, not whoAmI)
+        #     cg.makeMove(p.makeMove())
+        # if cg.isGameOver():
+        #     break
 
 if __name__ == "__main__":
     main()
